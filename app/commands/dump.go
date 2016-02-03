92919054c489acd39f6678fc03626f15e0362e64
madhead <siarhei.krukau@gmail.com>
2016-02-03 16:25:48 +0300

    Simplify dump operation

--- app/commands/dump.go
+++ app/commands/dump.go
@@ -13,12 +13,6 @@ import (
 	"github.com/paypal/gatt"
 )
 
-type peripheralResult struct {
-	peripheral    gatt.Peripheral
-	advertisement *gatt.Advertisement
-	rssi          int
-}
-
 // Dump dumps target BLE bulb to stdout
 func Dump() {
 	device, err := util.OpenHCI()
@@ -27,90 +21,95 @@ func Dump() {
 		os.Exit(util.ExitStatusHCIError)
 	}
 
-	peripheralFound := make(chan *peripheralResult)
 	done := make(chan bool)
 
 	device.Handle(gatt.PeripheralDiscovered(func(peripheral gatt.Peripheral, advertisement *gatt.Advertisement, rssi int) {
 		if strings.ToUpper(*cli.DumpTarget) == strings.ToUpper(peripheral.ID()) {
-			peripheralFound <- &peripheralResult{
-				peripheral:    peripheral,
-				advertisement: advertisement,
-				rssi:          rssi,
-			}
-		}
-	}))
-	device.Handle(gatt.PeripheralConnected(func(peripheral gatt.Peripheral, err error) {
-		services, err := peripheral.DiscoverServices(nil)
+			log.Info.Println("Target found:")
+			log.Info.Printf("\tID: %s, Name: %v\n", peripheral.ID(), peripheral.Name())
+			log.Info.Printf("\tLocal Name: %v\n", advertisement.LocalName)
+			log.Info.Printf("\tTX Power Level: %v\n", advertisement.TxPowerLevel)
+			log.Info.Printf("\tManufacturer Data: %v\n", advertisement.ManufacturerData)
+			log.Info.Printf("\tService Data: %v\n", advertisement.ServiceData)
 
-		if err != nil {
-			log.Error.Printf("Service discovering error: %v\n", err)
-			return
-		}
+			device.Handle(gatt.PeripheralConnected(func(peripheral gatt.Peripheral, err error) {
+				defer device.CancelConnection(peripheral)
 
-		for _, service := range services {
-			msg := "Service: " + service.UUID().String()
-
-			if len(service.Name()) > 0 {
-				msg += " (" + service.Name() + ")"
-			}
-			log.Info.Println(msg)
-
-			characteristics, err := peripheral.DiscoverCharacteristics(nil, service)
-
-			if err != nil {
-				log.Error.Printf("Characteristics discovering error: %v\n", err)
-				continue
-			}
-
-			for _, characteristic := range characteristics {
-				msg := "\tCharacteristic:  " + characteristic.UUID().String()
-
-				if len(characteristic.Name()) > 0 {
-					msg += " (" + characteristic.Name() + ")"
-				}
-				log.Info.Println(msg)
-				log.Info.Printf("\t\tProperties: %s\n", characteristic.Properties().String())
-
-				if (characteristic.Properties() & gatt.CharRead) != 0 {
-					value, err := peripheral.ReadCharacteristic(characteristic)
-
-					if err != nil {
-						log.Error.Printf("Failed to read characteristic: %v\n", err)
-						continue
-					}
-
-					log.Info.Printf("\t\tValue: %X (%q)\n", value, value)
-				}
-
-				// Discovery descriptors
-				descriptors, err := peripheral.DiscoverDescriptors(nil, characteristic)
+				services, err := peripheral.DiscoverServices(nil)
 
 				if err != nil {
-					log.Error.Printf("Failed to discover descriptors: %v\n", err)
-					continue
+					log.Error.Printf("Service discovering error: %v\n", err)
+					return
 				}
 
-				for _, descriptor := range descriptors {
-					msg := "\t\tDescriptor: " + descriptor.UUID().String()
+				for _, service := range services {
+					msg := "Service: " + service.UUID().String()
 
-					if len(descriptor.Name()) > 0 {
-						msg += " (" + descriptor.Name() + ")"
+					if len(service.Name()) > 0 {
+						msg += " (" + service.Name() + ")"
 					}
 					log.Info.Println(msg)
 
-					value, err := peripheral.ReadDescriptor(descriptor)
+					characteristics, err := peripheral.DiscoverCharacteristics(nil, service)
 
 					if err != nil {
-						fmt.Printf("Failed to read descriptor, err: %s\n", err)
+						log.Error.Printf("Characteristics discovering error: %v\n", err)
 						continue
 					}
-					log.Info.Printf("\t\t\tValue: %X (%q)\n", value, value)
-				}
-			}
-		}
 
-		peripheral.Device().CancelConnection(peripheral)
-		done <- true
+					for _, characteristic := range characteristics {
+						msg := "\tCharacteristic:  " + characteristic.UUID().String()
+
+						if len(characteristic.Name()) > 0 {
+							msg += " (" + characteristic.Name() + ")"
+						}
+						log.Info.Println(msg)
+						log.Info.Printf("\t\tProperties: %s\n", characteristic.Properties().String())
+
+						if (characteristic.Properties() & gatt.CharRead) != 0 {
+							value, err := peripheral.ReadCharacteristic(characteristic)
+
+							if err != nil {
+								log.Error.Printf("Failed to read characteristic: %v\n", err)
+								continue
+							}
+
+							log.Info.Printf("\t\tValue: %X (%q)\n", value, value)
+						}
+
+						// Discovery descriptors
+						descriptors, err := peripheral.DiscoverDescriptors(nil, characteristic)
+
+						if err != nil {
+							log.Error.Printf("Failed to discover descriptors: %v\n", err)
+							continue
+						}
+
+						for _, descriptor := range descriptors {
+							msg := "\t\tDescriptor: " + descriptor.UUID().String()
+
+							if len(descriptor.Name()) > 0 {
+								msg += " (" + descriptor.Name() + ")"
+							}
+							log.Info.Println(msg)
+
+							value, err := peripheral.ReadDescriptor(descriptor)
+
+							if err != nil {
+								fmt.Printf("Failed to read descriptor, err: %s\n", err)
+								continue
+							}
+							log.Info.Printf("\t\t\tValue: %X (%q)\n", value, value)
+						}
+					}
+				}
+
+				done <- true
+			}))
+
+			device.StopScanning()
+			device.Connect(peripheral)
+		}
 	}))
 
 	log.Info.Println("Scanning devices")
@@ -118,23 +117,9 @@ func Dump() {
 
 	select {
 	case <-time.After(*cli.ScanPeriod):
-		log.Error.Println("Target device not found")
-		device.StopScanning()
-		os.Exit(util.ExitStatusTargetDeviceNotFound)
-	case peripheralResult := <-peripheralFound:
-		device.StopScanning()
-		log.Info.Println("Target found:")
-		log.Info.Printf("\tID: %s, Name: %v\n", peripheralResult.peripheral.ID(), peripheralResult.peripheral.Name())
-		log.Info.Printf("\tLocal Name: %v\n", peripheralResult.advertisement.LocalName)
-		log.Info.Printf("\tTX Power Level: %v\n", peripheralResult.advertisement.TxPowerLevel)
-		log.Info.Printf("\tManufacturer Data: %v\n", peripheralResult.advertisement.ManufacturerData)
-		log.Info.Printf("\tService Data: %v\n", peripheralResult.advertisement.ServiceData)
-		device.Connect(peripheralResult.peripheral)
-	}
-	select {
-	case <-time.After(*cli.ScanPeriod):
-		log.Error.Println("Target device failed to be dumped")
-		os.Exit(util.ExitStatusTargetDeviceFailedToDump)
+		log.Error.Println("Failed to dump target device")
+		os.Exit(util.ExitStatusGenericError)
 	case <-done:
+		log.Info.Println("Target device dumped")
 	}
 }
--- app/util/exits.go
+++ app/util/exits.go
@@ -3,10 +3,6 @@ package util
 const (
 	// ExitStatusHCIError signals about errors with HCI device
 	ExitStatusHCIError = 1
-	// ExitStatusTargetDeviceNotFound signals about errors with discovering target device
-	ExitStatusTargetDeviceNotFound = 1
-	// ExitStatusTargetDeviceFailedToDump signals about errors with dumping target device
-	ExitStatusTargetDeviceFailedToDump = 1
 	// ExitStatusGenericError signals about generic error
 	ExitStatusGenericError = 1
 )
